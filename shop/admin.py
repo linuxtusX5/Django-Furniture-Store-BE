bf4f34073a177d5323f8d7239c149bca063c5242
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-12-08 11:19:46 +0800

    admin category registered

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product, Customer, Order, OrderItem, Review
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    lst_display = ['name', 'created_at', 'products_count']
+    search_fields = ['name', 'description']
+    list_filter = ['created_at']
+    ordering = ['name']
+
+    def products_count(self, obj):
+        return obj.products.count()
+    products_count.short_description = 'Products'
+
+
